76ef930c2b410a4f3840b149f331a6bc432c00cc
Thiago Dias <tdias7@outlook.com>
2022-05-02 23:02:06 -0300

    feat: atualiza teste

--- math_test.go
+++ math_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 	if total != 30 {
-		t.Errorf("resultado da soma eh invalido: %d,  esperado: %d", total, 30)
+		t.Errorf("resultado da soma é invalido: %d,  esperado: %d", total, 30)
 	}
 }
